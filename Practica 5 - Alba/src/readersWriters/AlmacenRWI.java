e4e075265f581e984f65788f84022e268888b1b2
Echoalizer <echoalizr@gmail.com>
2026-04-25 18:51:01 +0200

    Eliminadas clases del model rw que no necesitamos

--- Practica 5 - Alba/src/readersWriters/AlmacenRWI.java
+++ Practica 5 - Alba/src/readersWriters/AlmacenRWI.java
@@ -4,10 +4,10 @@ package readersWriters;
 import java.io.Serializable;
 import java.util.List;
 
-///
-/// Clase que representa un almacén para el modelo de lectores y escritores.
-/// Métodos para añadir y borrar un elemento, y para obtener todos los de la lista.
-///
+/**
+ * Clase que representa un almacén para el modelo de lectores y escritores.
+ * Métodos para añadir y borrar un elemento, y para obtener todos los de la lista.
+ */
 public interface AlmacenRWI<T extends Serializable> {
 
     public boolean escribir(T producto, String key) throws InterruptedException;
